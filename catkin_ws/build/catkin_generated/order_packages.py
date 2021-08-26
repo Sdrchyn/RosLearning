0c9878b1191794e1d32e9125f8882f11226fb2c3
mars <Sdrchyn@163.com>
2021-08-26 22:25:36 +0800

    first commit

---
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/mars/Documents/github/RosLearning/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/mars/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/mars/catkin_ws/devel;/opt/ros/noetic' != '' else []
---
+++ catkin_ws/build/learning_topic/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/mars/Documents/github/RosLearning/catkin_ws/src/learning_topic
+# Build directory: /home/mars/Documents/github/RosLearning/catkin_ws/build/learning_topic
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
